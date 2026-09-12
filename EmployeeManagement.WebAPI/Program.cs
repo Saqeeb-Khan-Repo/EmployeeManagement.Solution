c2c04a6ac468755b73b0bc5186bfdca137f38f35
Mr khan <khansaqeeb332@gmail.com>
2026-09-12 11:55:00 +0530

    added xml comments for home controller

--- EmployeeManagement.WebAPI/Program.cs
+++ EmployeeManagement.WebAPI/Program.cs
@@ -4,61 +4,60 @@ using Microsoft.EntityFrameworkCore;
 using ServiceContracts.WebAPI;
 using Services.WebAPI;
 
-namespace EmployeeManagement.WebAPI
+namespace EmployeeManagement.WebAPI;
+
+public class Program
 {
-    public class Program
+    /// <summary>
+    /// This is the static Main method in Program.cs file
+    /// </summary>
+    /// <param name="args"></param>
+    public static void Main(string[] args)
     {
-        /// <summary>
-        /// This is the static Main method in Program.cs file
-        /// </summary>
-        /// <param name="args"></param>
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers(options =>
         {
-            var builder = WebApplication.CreateBuilder(args);
+            //Added in swagger to force only appli/json formate
+            options.Filters.Add(new ProducesAttribute("application/json"));
+            options.Filters.Add(new ConsumesAttribute("application/json"));
+        });
 
-            // Add services to the container.
-
-            builder.Services.AddControllers(options =>
+        builder.Services.AddOpenApi();
+        builder.Services.AddDbContext<ApplicationDBContext>(
+            options =>
             {
-                //Added in swagger to force only appli/json formate
-                options.Filters.Add(new ProducesAttribute("application/json"));
-                options.Filters.Add(new ConsumesAttribute("application/json"));
-            });
-
-            builder.Services.AddOpenApi();
-            builder.Services.AddDbContext<ApplicationDBContext>(
-                options =>
-                {
-                    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
-                }
-                );
-            builder.Services.AddScoped<IEmployeeService, EmployeeService>();
-
-            //swagger
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen(options =>
-            {
-                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "api.xml"));
-            });
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if(app.Environment.IsDevelopment())
-            {
-                app.MapOpenApi();
+                options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
             }
+            );
+        builder.Services.AddScoped<IEmployeeService, EmployeeService>();
 
-            //Swagger 
-            app.UseSwagger(); //use swagger.json
-            app.UseSwaggerUI(); //enable swagger ui
+        //swagger
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(options =>
+        {
+            options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "api.xml"));
+        });
 
-            app.UseHttpsRedirection();
-            app.UseAuthorization();
+        var app = builder.Build();
 
-            //map controllers routes
-            app.MapControllers();
-            app.Run();
+        // Configure the HTTP request pipeline.
+        if(app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
         }
+
+        //Swagger 
+        app.UseSwagger(); //use swagger.json
+        app.UseSwaggerUI(); //enable swagger ui
+
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+
+        //map controllers routes
+        app.MapControllers();
+        app.Run();
     }
 }
